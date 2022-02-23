55a631c1ad24412bc561abdfb5e0fd9416a6c93f
breno santos <breno.angelim@hotmail.com>
2022-02-23 01:19:07 -0300

    structure first flask project

---
+++ project/__init__.py
@@ -0,0 +1,17 @@
+# minha_aplicacao.py
+        
+from crypt import methods
+from flask import Flask
+from os import getenv
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Bem vindo ao flask!'
+
+@app.route('/primeira', methods=['GET'])
+def primeira_rota():
+    user = getenv('DB_USERNAME')
+    return {'msg': 'primeira rota', 'user': user}
