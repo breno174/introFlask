526ae880a3741e7cd0a7e574ebac7a96b050b273
breno santos <breno.angelim@hotmail.com>
2022-02-24 10:37:00 -0300

    segunda rota

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,7 @@
 # minha_aplicacao.py
 
 from crypt import methods
-from flask import Flask
+from flask import Flask, request
 from os import getenv
 
 
@@ -15,3 +15,14 @@ def home():
 def primeira_rota():
     user = getenv('DB_USERNAME')
     return {'msg': 'primeira rota', 'user': user}
+
+
+@app.get('/segunda/query_params')
+def segunda_rota():
+    nome = request.args
+    #Para: /segunda/query_params?valor_1=russia&valor_2=ucrania
+    #retorna Bem vindo ImmutableMultiDict([('valor_1', 'russia'), ('valor_2', 'ucrania')])!
+    if nome:
+        return {'msg': f'Bem vindo {nome}!'}
+    
+    return {'msg': 'Status OK!'}
